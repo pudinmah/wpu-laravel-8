f276c78a8827b8a163e28e712cea991497792a0d
Mahpudin <pudinmah69@gmail.com>
2023-12-15 17:40:25 +0700

    Belajar Laravel 8 | 3. Struktur Folder, Routes & View

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3><?php echo $name ?> </h3>
+    <p><?= $email ?></p>
+    <img src="img/people1.jpg" alt="">
+</body>
+</html>
