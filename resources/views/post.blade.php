2b1a2945dd5ef7e5d20a01530f0c2371d511320d
Mahpudin <pudinmah69@gmail.com>
2023-12-16 21:39:22 +0700

    4. Blade Templating Engine

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU | Halaman Posts</title>
-</head>
-<body>
-    <h1>Halaman Post</h1>
-</body>
-</html>
+{{-- @dd($post); --}}
+
+@extends('layouts.main')
+
+@section('container')
+<article>
+    <h2>{{$post["title"]}}</h2>
+    <h5>{{$post["author"]}}</h5>
+    <p>{{$post["body"]}} </p>
+
+    </article>
+    <a href="/blog">Back to post</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,74 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "home"
+    ]);
 });
 Route::get('/about', function () {
-    return view('about',
-    [
-        "name"=> "Dodi",
-        "email"=>"pudinmah4@gmail.com",
-        "image"=>"people1.jpg"
-    ]
-);
+    return view(
+        'about',
+        [
+            "title" => "about",
+            "name" => "Mahpudin",
+            "email" => "pudinmah4@gmail.com",
+            "image" => "people2.jpg"
+        ]
+    );
 });
+
+
+
 Route::get('/blog', function () {
-    return view('post');
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Mahpudin",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam recusandae, nostrum error quas modi cupiditate cumque voluptate ipsa itaque et delectus voluptates eveniet aliquam illo in blanditiis vero praesentium magni totam tempore, voluptatum nobis dolor provident? Quis libero sequi hic excepturi, dolorum vitae tempore soluta dignissimos odit praesentium dolorem qui, veritatis id fuga voluptate enim numquam, neque voluptates consectetur officia esse sed dicta maiores. Adipisci, repudiandae doloremque! In eum libero quidem sit consectetur laborum velit totam dolorem error autem.",
+        ],
+        [
+            "title" => "Judul Post Dua",
+            "slug"=> "judul-post-kedua",
+            "author" => "Dodi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam recusandae, nostrum error quas modi cupiditate cumque voluptate ipsa itaque et delectus voluptates eveniet aliquam illo in blanditiis vero praesentium magni totam tempore, voluptatum nobis dolor provident? Quis libero sequi hic excepturi, dolorum vitae tempore soluta dignissimos odit praesentium dolorem qui, veritatis id fuga voluptate enim numquam, neque voluptates consectetur officia esse sed dicta maiores. Adipisci, repudiandae doloremque! In eum libero quidem sit consectetur laborum velit totam dolorem error autem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam recusandae, nostrum error quas modi cupiditate cumque voluptate ipsa itaque et delectus voluptates eveniet aliquam illo in blanditiis vero praesentium magni totam tempore, voluptatum nobis dolor provident? Quis libero sequi hic excepturi, dolorum vitae tempore soluta dignissimos odit praesentium dolorem qui, veritatis id fuga voluptate enim numquam, neque voluptates consectetur officia esse sed dicta maiores. Adipisci, repudiandae doloremque! In eum libero quidem sit consectetur laborum velit totam dolorem error autem.",
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "post",
+        "posts"=>$blog_post,
+    ]);
+});
+
+
+// halaman singgle post
+Route::get('posts/{slug}', function($slug){
+
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Mahpudin",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam recusandae, nostrum error quas modi cupiditate cumque voluptate ipsa itaque et delectus voluptates eveniet aliquam illo in blanditiis vero praesentium magni totam tempore, voluptatum nobis dolor provident? Quis libero sequi hic excepturi, dolorum vitae tempore soluta dignissimos odit praesentium dolorem qui, veritatis id fuga voluptate enim numquam, neque voluptates consectetur officia esse sed dicta maiores. Adipisci, repudiandae doloremque! In eum libero quidem sit consectetur laborum velit totam dolorem error autem.",
+        ],
+        [
+            "title" => "Judul Post Dua",
+            "slug"=> "judul-post-kedua",
+            "author" => "Dodi",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam recusandae, nostrum error quas modi cupiditate cumque voluptate ipsa itaque et delectus voluptates eveniet aliquam illo in blanditiis vero praesentium magni totam tempore, voluptatum nobis dolor provident? Quis libero sequi hic excepturi, dolorum vitae tempore soluta dignissimos odit praesentium dolorem qui, veritatis id fuga voluptate enim numquam, neque voluptates consectetur officia esse sed dicta maiores. Adipisci, repudiandae doloremque! In eum libero quidem sit consectetur laborum velit totam dolorem error autem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi magnam recusandae, nostrum error quas modi cupiditate cumque voluptate ipsa itaque et delectus voluptates eveniet aliquam illo in blanditiis vero praesentium magni totam tempore, voluptatum nobis dolor provident? Quis libero sequi hic excepturi, dolorum vitae tempore soluta dignissimos odit praesentium dolorem qui, veritatis id fuga voluptate enim numquam, neque voluptates consectetur officia esse sed dicta maiores. Adipisci, repudiandae doloremque! In eum libero quidem sit consectetur laborum velit totam dolorem error autem.",
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_post as $post) {
+        if($post["slug"]==$slug){
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title"=> "Single Post",
+        "post"=> $new_post
+    ]);
 });
